d6c51aa8f94726eb37d89994b123c9d937e4f380
Kurt Culley <kurt.culley@gmail.com>
2017-08-01 20:15:17 +0100

    Updated to work with new backend API changes.

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const ROOT_URL = 'https://ancient-tundra-28811.herokuapp.com/restaurants';
+const ROOT_URL = 'https://ancient-tundra-28811.herokuapp.com';
 
 export const fetchCategories = () => {
-    const URI = `${ROOT_URL}/${localStorage.getItem('restaurantId')}/menu_categories`;
+    const URI = `${ROOT_URL}/restaurants/${localStorage.getItem('restaurantId')}/menu_categories`;
     return fetch(URI)
         .then(response => {
             return response.json();
@@ -18,7 +18,7 @@ export const fetchCategories = () => {
 };
 
 export const fetchItems = (categoryId) => {
-    const URI = `${ROOT_URL}/${localStorage.getItem('restaurantId')}/menu_categories/${categoryId}/menu_items`;
+    const URI = `${ROOT_URL}/menu_categories/${categoryId}/menu_items`;
     return fetch(URI)
         .then(response => {
             return response.json();
@@ -35,7 +35,7 @@ export const fetchItems = (categoryId) => {
 };
 
 export const fetchOrder = (orderId) => {
-    const URI = `${ROOT_URL}/${localStorage.getItem('restaurantId')}/orders/${orderId}`;
+    const URI = `${ROOT_URL}/orders/${orderId}`;
     return fetch(URI)
         .then(response => {
             return response.json();
@@ -43,27 +43,27 @@ export const fetchOrder = (orderId) => {
 };
 
 export const addToOrder = (itemId) => {
-    const URI = `${ROOT_URL}/${localStorage.getItem('restaurantId')}/orders/${localStorage.getItem('orderId')}/items`;
+    const URI = `${ROOT_URL}/orders/${localStorage.getItem('orderId')}/items`;
     return axios.post(URI, { menu_item_id: itemId });
 };
 
 export const deleteOrderItem = (itemId) => {
-    const URI = `${ROOT_URL}/${localStorage.getItem('restaurantId')}/orders/${localStorage.getItem('orderId')}/items/${itemId}`;
+    const URI = `${ROOT_URL}/orders/${localStorage.getItem('orderId')}/items/${itemId}`;
     return axios.delete(URI);
 };
 
 export const increaseOrderItem = (itemId) => {
-    const URI = `${ROOT_URL}/${localStorage.getItem('restaurantId')}/orders/${localStorage.getItem('orderId')}/items/${itemId}/add`;
+    const URI = `${ROOT_URL}/orders/${localStorage.getItem('orderId')}/items/${itemId}/add`;
     return axios.post(URI);
 };
 
 export const reduceOrderItem = (itemId) => {
-    const URI = `${ROOT_URL}/${localStorage.getItem('restaurantId')}/orders/${localStorage.getItem('orderId')}/items/${itemId}/reduce`;
+    const URI = `${ROOT_URL}/orders/${localStorage.getItem('orderId')}/items/${itemId}/reduce`;
     return axios.post(URI);
 };
 
 export const createOrder = (restaurantId, tableId) => {
-    return axios.post(`${ROOT_URL}/${restaurantId}/orders`, { table_id: tableId })
+    return axios.post(`${ROOT_URL}/tables/${tableId}/orders`)
         .then(function (response) {
             localStorage.setItem("orderId", response.data.id);
             localStorage.setItem("restaurantId", restaurantId);
@@ -71,17 +71,17 @@ export const createOrder = (restaurantId, tableId) => {
 };
 
 export const deleteOrder = (orderId) => {
-    const URI = `${ROOT_URL}/${localStorage.getItem('restaurantId')}/orders/${orderId}`;
+    const URI = `${ROOT_URL}/orders/${orderId}`;
     return axios.delete(URI).then(localStorage.clear());
 };
 
 export const checkoutOrder = (nonce) => {
-    const URI = `${ROOT_URL}/${localStorage.getItem('restaurantId')}/orders/${localStorage.getItem('orderId')}/payment`;
+    const URI = `${ROOT_URL}/orders/${localStorage.getItem('orderId')}/payment`;
     return axios.post(URI, { payment_method_nonce: nonce});
 };
 
 export const getPaymentToken = () => {
-    const URI = `${ROOT_URL}/${localStorage.getItem('restaurantId')}/orders/${localStorage.getItem('orderId')}/payment/client_token`;
+    const URI = `${ROOT_URL}/orders/${localStorage.getItem('orderId')}/payment/client_token`;
     return fetch(URI)
         .then(
         function(response){
